3814ef32e58e5d75c3e453e578bb06fad96419bb
mrvnmnsld <40486868+mrvnmnsld@users.noreply.github.com>
2022-05-30 23:46:52 +0800

    resolved slider
    resolved slider  in root.index and loginform.php

--- application/views/index.php
+++ application/views/index.php
@@ -201,25 +201,25 @@
 
 		  	<div class="text-center bg-dark text-danger mb-3" id="errorReporter"></div>
 
-		  	<button type="button" data-toggle="modal" data-target="#sliderCaptchaModal" class="btn btn-success col-md-12">Login</button>
+		  	<button id="submit_login_btn" type="button" data-toggle="modal" data-target="#sliderCaptchaModal" class="btn btn-success col-md-12">Login</button>
 
 		  	<br>
 		  	
 		  	<span>
-		  		<button type="button" id="signUpBtn" class="btn btn-link text-primary"><u>Not yet a member? Click here</u></button>
+		  		<button id="signUpBtn" class="btn btn-link text-primary" href="#signUp"><u>Not yet a member? Click here</u></button>
 		  	</span>
 		</form>
 	</div>
 </div>
 
-			<!-- sliderCaptchaModal -->
-			<div class="modal fade" id="sliderCaptchaModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-				<div class="modal-dialog modal-dialog-centered">
-					<div class="modal-content" style="background-color:transparent; border:none;">
-						<div id="captcha"></div>
-					</div>
-				</div>
-			</div>
+<!-- sliderCaptchaModal -->
+<div class="modal fade" id="sliderCaptchaModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered">
+		<div class="modal-content" style="background-color:transparent; border:none;">
+		    <div id="captcha"></div>
+		</div>
+	</div>
+</div>
 
 <script type="text/javascript">
 	var currentUser = JSON.parse(getLocalStorageByKey('currentUser'));
@@ -234,7 +234,6 @@
 			window.location.replace("homeViewPro");
 		}
 		
-
 	}else{
 		console.log("no active user")
 	}
@@ -245,6 +244,33 @@
 		$("#container").empty();
 		$("#container").append(res);
 	}	
+
+	var captcha = sliderCaptcha({
+		id: 'captcha',
+		width: 280,
+		height: 155,
+		sliderL: 42,
+		sliderR: 9,
+		offset: 5,
+		loadingText: 'Loading...',
+		failedText: 'Try again',
+		barText: 'Slide right to fill',
+		repeatIcon: 'fa fa-redo',
+		onSuccess: function () {
+			$("#sliderCaptchaModal").css("display", 'none')
+			$(".modal-backdrop").css("display", 'none')
+			console.log("captcha slider success");
+
+			$("#loginForm").submit();
+		},
+		onFail: function () {
+			// captcha.reset();
+		},
+		onRefresh: function () {
+			// captcha.reset();
+		}
+	});
+
 
 	// $.validator.addMethod("captchaCheck",function(value, element) {
 	// 		return value==captchaRight;
@@ -259,12 +285,13 @@
 			password: "required",
 	  	},
 	  	submitHandler: function(form){
+	  		captcha.reset();
+
 		    var data = $('#loginForm').serializeArray();
 	    	data.push({"name":'ip','value':getIpAddress()["ip"]});
 
-
 		    var loginRes = ajaxShortLink('checkLoginCredentials',data);
-
+		    
 	  		if (loginRes['wrongFlag'] == 2 || loginRes['wrongFlag'] == 1) {
 	  		  $('#errorReporter').text("Wrong Credentials.");
 	  		}else if(loginRes['wrongFlag'] == 3){
@@ -272,12 +299,12 @@
 	  		}else if(loginRes['wrongFlag'] == 0){
 	  			setLocalStorageByKey('currentUser',JSON.stringify(loginRes['data']));
 
-	  				if(loginRes.data.isPro == 0){
-						window.location.replace("homeView");
-					}
-					else {
-						window.location.replace("homeViewPro");
-					}
+  				if(loginRes.data.isPro == 0){
+					window.location.replace("homeView");
+				}
+				else {
+					window.location.replace("homeViewPro");
+				}
 
 	  		}
 	  	}
@@ -291,43 +318,7 @@
 
 	});
 
-	// sliderCatpcha JS menu
-
-
-	sliderCaptcha({
-				id: 'captcha',
-				width: 280,
-				height: 155,
-				sliderL: 42,
-				sliderR: 9,
-				offset: 5,
-				loadingText: 'Loading...',
-				failedText: 'Try again',
-				barText: 'Slide right to fill',
-				repeatIcon: 'fa fa-redo',
-
-				// localImages: function () { // uses local images instead
-					
-				// },
-				// setSrc: function () {
-				
-				// },
-
-				onSuccess: function () {
-					$("#sliderCaptchaModal").css("display", 'none')
-					$(".modal-backdrop").css("display", 'none')
-					console.log("captcha slider success");
-
-					$("#loginForm").submit();
-				},
-				onFail: function () {
-
-				},
-				onRefresh: function () {
-				
-				}
-			});
-
+	
 			
 	// submit_login_btn
 	$("#submit_login_btn").on("click",function(){
@@ -335,6 +326,7 @@
 		// if success continue to validate
 		// validation 
 		// $("#loginForm").submit();
+		captcha.reset();
 	});
 
 </script>
