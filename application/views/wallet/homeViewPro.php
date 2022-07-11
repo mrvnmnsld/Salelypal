d0a8809efcf57299bd8b1f2e270034e8afcf9888
arlpple9 <62857719+arlpple9@users.noreply.github.com>
2022-07-11 21:33:57 +0800

    Update homeViewPro.php
    scroll bug fixed

--- application/views/wallet/homeViewPro.php
+++ application/views/wallet/homeViewPro.php
@@ -1857,8 +1857,8 @@
 
 
 
-
-			function swipedetect(el, callback){
+		//swipe detect
+		function swipedetect(el, callback){
 
 			var touchsurface = el,
 			swipedir,
@@ -1874,62 +1874,62 @@
 			handleswipe = callback || function(swipedir){}
 
 			touchsurface.addEventListener('touchstart', function(e){
-			var touchobj = e.changedTouches[0]
-			swipedir = 'none'
-			dist = 0
-			startX = touchobj.pageX
-			startY = touchobj.pageY
-			startTime = new Date().getTime() // record time when finger first makes contact with surface
-			e.preventDefault()
+				var touchobj = e.changedTouches[0]
+				swipedir = 'none'
+				dist = 0
+				startX = touchobj.pageX
+				startY = touchobj.pageY
+				startTime = new Date().getTime() // record time when finger first makes contact with surface
+				// e.preventDefault()
 			}, false)
 
-			touchsurface.addEventListener('touchmove', function(e){
-			e.preventDefault() // prevent scrolling when inside DIV
-			}, false)
+			// touchsurface.addEventListener('touchmove', function(e){
+			// 	e.preventDefault() // prevent scrolling when inside DIV
+			// }, false)
 
 			touchsurface.addEventListener('touchend', function(e){
-			var touchobj = e.changedTouches[0]
-			distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
-			distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
-			elapsedTime = new Date().getTime() - startTime // get time elapsed
-			if (elapsedTime <= allowedTime){ // first condition for awipe met
-			if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
-			swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
-			}
-			else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
-			swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
-			}
-			}
-			handleswipe(swipedir)
-			e.preventDefault()
+				var touchobj = e.changedTouches[0]
+				distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+				distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+				elapsedTime = new Date().getTime() - startTime // get time elapsed
+					if (elapsedTime <= allowedTime){ // first condition for awipe met
+						if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+							swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+						}
+						else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+							swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+						}
+				}
+				handleswipe(swipedir)
+				e.preventDefault()
 			}, false)
+		}
+
+
+		var balance_tab = document.getElementById('balance_tab');
+		swipedetect(balance_tab, function(balance_tab_swipe){
+			if (balance_tab_swipe =='left'){
+				$('#portfolio_tab').tab('show'); 
+				$('#balance_tab').removeClass('active');
+				$('#balance_tab').addClass('hide');
+				$('#portfolio_tab').addClass('active');
+
+				$('#portfolio_tab_id').addClass('active');
+				$('#balance_tab_id').removeClass('active');
 			}
-
-
-		var el = document.getElementById('balance_tab')
-		swipedetect(el, function(swipedir){
-		if (swipedir =='left'){
-			$('#portfolio_tab').tab('show'); 
-			$('#balance_tab').removeClass('active');
-			$('#balance_tab').addClass('hide');
-			$('#portfolio_tab').addClass('active');
-
-			$('#portfolio_tab_id').addClass('active');
-			$('#balance_tab_id').removeClass('active');
-		} 		
 		});
 
-		var el2 = document.getElementById('portfolio_tab')
-		swipedetect(el2, function(swipedir2){
-		if (swipedir2 =='right'){
-			$('#portfolio_tab').removeClass('active');
-			$('#portfolio_tab').addClass('hide');
-			$('#balance_tab').addClass('active');
-			$('#balance_tab').tab('show'); 
+		var portfolio_tab = document.getElementById('portfolio_tab');
+		swipedetect(portfolio_tab, function(portfolio_tab_swipe){
+			if (portfolio_tab_swipe =='right'){
+				$('#portfolio_tab').removeClass('active');
+				$('#portfolio_tab').addClass('hide');
+				$('#balance_tab').addClass('active');
+				$('#balance_tab').tab('show'); 
 
-			$('#balance_tab_id').addClass('active');
-			$('#portfolio_tab_id').removeClass('active');
-		} 		
+				$('#balance_tab_id').addClass('active');
+				$('#portfolio_tab_id').removeClass('active');
+			}
 		});
 	</script>
 </body>
