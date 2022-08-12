46b8ba42da833ecabdcb5c4cc15bb0dd8a563538
ch0pan <104147757+ch0pan@users.noreply.github.com>
2022-08-12 16:27:49 +0800

    terms and conditions updated

--- application/views/wallet/withdraw.php
+++ application/views/wallet/withdraw.php
@@ -589,10 +589,10 @@
 
    	function termsCondition(){
 		bootbox.alert({
-			title: "Terms and Condition",
-			centerVertical: true,
-		    message: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-		    size: 'large'
+	    message: ajaxLoadPage('quickLoadPage',{'pagename':'wallet/termsConditions'}),
+	    size: 'large',
+	    centerVertical: true,
+			closeButton: false
 		});
 	}
 
